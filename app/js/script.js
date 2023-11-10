3c80ef2813c6742032a66ce58cc80d417e6e6561
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-10 19:36:46 +0400

    dist

--- app/js/script.js
+++
@@ -1,8 +0,0 @@
-import swiper from './modules/swiper';
-import scroll from './modules/scroll';
-import form from './modules/form';
-import burger from './modules/burger';
-swiper();
-scroll();
-form();
-burger();
